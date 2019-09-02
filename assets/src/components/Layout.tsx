959ceb8e2e1d206b671c64c6d1842b4beccdc35e
ludovicm67 <ludovicmuller1@gmail.com>
2019-09-02 11:33:51 +0200

    added Bulma

---
+++ assets/src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Form from './Form';
+import '../../node_modules/bulma/css/bulma.min.css';
+
+export const Hello = () => (
+  <div>
+    <h1>Hello!</h1>
+    <Form />
+  </div>
+);
+
+export default Hello;
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (_env, _options) => ({
   mode: 'production',
   devtool: 'source-map',
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', '.css'],
   },
 
   output: {
@@ -36,6 +38,14 @@ module.exports = (_env, _options) => ({
         test: /\.js$/,
         loader: 'source-map-loader',
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+  ]
 });
